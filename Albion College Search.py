4647b6e867a1293df9e575377582f10c1432085d
TJHuck <goldensnitch821@gmail.com>
2026-02-04 12:17:41 -0500

    Added necessary imports, created a new graph, added nodes and edges, and drew the graph with matplotlib.

--- Albion College Search.py
+++ Albion College Search.py
@@ -1,3 +1,19 @@
+## NECCESSARY IMPORTS
+import matplotlib.pyplot as plt
+import networkx as nx
+
+##img = plt.imread("C:/Users/golde/OneDrive/Pictures/Screenshots/ALBION COLLEGE CAMPUS.png")
+G = nx.Graph()
+
+G.add_node("Node A")
+G.add_node("Node B")
+
+G.add_edge("Node A", "Node B")
+
+##plt.imshow(img)
+
+nx.draw(G, with_labels=True, node_color='skyblue', node_size=1000, edge_color='black', font_size=12)
+
+plt.show()
 ## Locations
 
-##Kresge
